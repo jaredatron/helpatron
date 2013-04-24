00c7e115e2f7a37c25dad1fb4e0a5ea887a6f7c5
Jared Grippe <jared@deadlyicon.com>
2013-04-24 10:40:58 -0700

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Helpatron::Application.config.secret_token = '7e93da59fd520e410198e0215532bb54af8bd401fb6b22a1dd2babcaa563bdef76def73f8e79498414cc0b769c64c43b8943362b478fb3868468f3f2c0910971'
